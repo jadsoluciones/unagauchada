9e54035f89e6ad95b3f45d2d7de977a7eadaf2c6
Augusto Bonifacio <augusto.bonifacio95@gmail.com>
2016-10-27 15:57:39 -0300

    Cambiado rango por puntaje

--- app/views/logros/_logro.json.jbuilder
+++ app/views/logros/_logro.json.jbuilder
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-json.extract! logro, :id, :titulo, :rango, :created_at, :updated_at
-=======
 json.extract! logro, :id, :titulo, :puntaje, :created_at, :updated_at
->>>>>>> augusto
 json.url logro_url(logro, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161026235509) do
+ActiveRecord::Schema.define(version: 20161027184130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "logros", force: :cascade do |t|
     t.string   "titulo"
-    t.integer  "rango"
+    t.integer  "puntaje"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
