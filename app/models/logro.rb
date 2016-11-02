ed80d0b3fc22a23c40b38b9d9c9833fd84a711d6
juanch014 <juancho_g014@hotmail.com>
2016-11-02 17:09:19 -0300

    Modifique el registro de usuarios y saque el privado a next

--- app/models/logro.rb
+++ app/models/logro.rb
@@ -4,15 +4,16 @@ class Logro < ActiveRecord::Base
 
 	before_destroy :check_for_unico
 
+	def next
+	  self.class.where("puntaje > ?", puntaje).first || Logro.new(titulo:'Inifinito',puntaje:'2147483648')
+	end
+
 	private
 
-	def check_for_unico   
-	  unless puntaje != -2147483648     
+	def check_for_unico
+	  unless puntaje != -2147483648
 	    self.errors[:base] << "Debe haber al menos un logro"
-	    return false   
-	  end 
-	end
-	def next
-	  self.class.where("puntaje > ?", puntaje).first || Logro.new(name:'Inifinito',puntaje:'2147483648')
+	    return false
+	  end
 	end
 end
