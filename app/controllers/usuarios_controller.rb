9dff1580384cb9c4e564066b54d0bc095e0586db
juanch014 <juancho_g014@hotmail.com>
2016-11-14 22:09:50 -0300

    Agregue puntaje y logro al show

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,7 +1,10 @@
 class UsuariosController < ApplicationController
 
+
+
   def show
     @usuario = Usuario.find(params[:id])
+    @puntaje = Logro.where("min < ?", @usuario.puntos).order(titulo: :desc).first
   end
 
 end
