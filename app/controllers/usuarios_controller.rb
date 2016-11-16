dbccfb03f1113954eee6da7a631361577bf0bb43
Augusto Bonifacio <augusto.bonifacio95@gmail.com>
2016-11-16 08:51:26 -0300

    Movido logro de usuario al Modelo

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -4,7 +4,6 @@ class UsuariosController < ApplicationController
 
   def show
     @usuario = Usuario.find(params[:id])
-    @puntaje = Logro.where("min <= ?", @usuario.puntos).order(min: :desc).first
   end
 
 end
