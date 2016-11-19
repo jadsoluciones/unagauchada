b9dc11962a602e610dbe7fa779115ff32ef81e1d
Augusto Bonifacio <augusto.bonifacio95@gmail.com>
2016-11-19 19:08:12 -0300

    Restriccion a subida de archivos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,7 +32,18 @@ $(document).ready(function() {
 		});
 	});
 
+	// $('.file-path').change(function(){
+		
+ //    	var allowed = ['','GIF','PNG','JPG','BMP'];
 
+ //    	if(allowed.indexOf( $(this).val().split('.').pop().toUpperCase() ) < 0){
+ //    		alert('Debe subir un archivo de tipo imagen');
+ //    		$('button[type="submit"]').prop( "disabled", true );
+ //    	}else{
+ //    		$('button[type="submit"]').prop( "disabled", false );
+ //    	}
+
+	// });
 	// $('.datepicker').pickadate({
 	// 	monthsFull: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
 	// 	monthsShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+	def img_exts
+		allowed = ['.GIF','.PNG','.JPG','.BMP']
+		allowed = allowed.concat allowed.map(&:downcase)
+		allowed.join ','
+	end
 end
