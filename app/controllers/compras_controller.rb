a960e044aea1312351b990f74257b742eac30671
Augusto Bonifacio <augusto.bonifacio95@gmail.com>
2016-11-21 20:04:17 -0300

    Los puntos comprados se suman al usuario

--- app/controllers/compras_controller.rb
+++ app/controllers/compras_controller.rb
@@ -43,6 +43,11 @@ class ComprasController < ApplicationController
 
     respond_to do |format|
       if valid_card? && @compra.save
+
+        usuario = Usuario.find(current_usuario.id)
+        usuario.puntos += puntos.to_i
+        usuario.save
+
         format.html { redirect_to @compra, notice: "Usted compró #{@compra.puntos_comprados} puntos por un total de $ARS #{@compra.monto}." }
         format.json { render :show, status: :created, location: @compra }
       else
