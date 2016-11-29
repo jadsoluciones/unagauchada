893e41182161bb376969a7c4e4ae85109ab65041
arambarrid <delfinarambarri@gmail.com>
2016-11-29 19:01:59 -0300

    Validacion de contenido en pregs y rtas

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,7 @@ class Question < ActiveRecord::Base
 	belongs_to :gauchada
 	belongs_to :usuario
 	has_one :answer, dependent: :destroy
+	validates :contenido, presence: true
+	validates :usuario, presence: true
+	validates :gauchada, presence: true
 end
